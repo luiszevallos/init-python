18f7aff291b4ab62341dda53438463edcd3fa657
lzevallos-android <luis_zevallos_17@hotmail.com>
2022-01-13 23:20:56 -0400

    login

---
+++ generator_password.py
@@ -0,0 +1,30 @@
+
+
+def login_user():
+    data_user = [
+        {
+            "user": "lzevallos",
+            "password": "12345",
+            "name": "Luis"
+        }
+    ]
+    user_name = input("Ingrese usuario: ")   
+    user_password = input("Ingrese contraseña: ")   
+    data = list(filter(lambda us: us["user"] == user_name and us["password"] == user_password, data_user))
+    if len(data) > 0:
+        
+        return True
+    else:
+        return False
+
+
+def run():
+    auth = login_user()
+    if auth:
+        print("Esta logeado")
+    else:
+        print("Usuario o contraseña incorrecta")
+
+
+if __name__ == "__main__":
+    run()
